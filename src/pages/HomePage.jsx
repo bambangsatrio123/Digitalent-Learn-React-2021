1058eafb5ef01b2cf32e9dfb9b524adc7cbcc3af
Bambang Satrio Gandhi <dandisatrio02@gmail.com>
2021-09-27 16:43:02 +0700

    implementasi ke ant design

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,43 @@
+import { Button, Modal, Typography, Form, Input, DatePicker } from "antd";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import useUser from "../hooks/useUser";
 
+const { Title } = Typography;
+
 export default function HomePage() {
+  const showModal = () => {
+    Modal.confirm({
+      title: <Button>Ini Modal</Button>,
+      content: (
+        <Form layout="vertical">
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+        </Form>
+      ),
+    });
+  };
+
   const [user] = useUser();
+  const [date, setDate] = useState();
+
+  function onChange(date, dateString) {
+    setDate(dateString);
+  }
+
   return (
     <div className="App">
-      <h1>Halaman Utama</h1>
+      <Title>Halaman Utama</Title>
       <h4>Halo {user.name}</h4>
       {user.isLogged ? (
         <p>
@@ -18,10 +50,18 @@ export default function HomePage() {
         <p>
           Silahkan Login{" "}
           <Link to="/login">
-            <button>Disini</button>
+            <Button type="primary" style={{ borderTopLeftRadius: 16, borderTopRightRadius: 16 }}>
+              Disini
+            </Button>
           </Link>
         </p>
       )}
+      <Button type="ghost" onClick={showModal}>
+        Show Modal
+      </Button>
+      <DatePicker onChange={onChange} format="DD MMM YYYY" />
+
+      <Typography.Title level={3}>{date}</Typography.Title>
     </div>
   );
 }
